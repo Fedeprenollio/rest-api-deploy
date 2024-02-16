a73d3c12083ddd4c9e9a4afc448a1983bf7c751a
Fedeprenollio <fede.preno@gmail.com>
2024-02-15 23:24:12 -0300

    fixed error cors

--- app.js
+++ app.js
@@ -27,19 +27,19 @@ const WHITELISTE = ['http://localhost:8080', 'http://localhost:5500']
 const corsOptions = {
   origin: function (origin, callback) {
     console.log('ORIGINS', origin)
-    // if (WHITELISTE.includes(origin)) {
-    //   return callback(null, true)
-    // }
-
-    // if (!origin) {
-    //   return callback(new Error('Not allowed by CORS1'))
-    // }
-    // return callback(new Error('Not allowed by CORS2'))
-    if (WHITELISTE.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
+    if (WHITELISTE.includes(origin)) {
+      return callback(null, true)
     }
+
+    if (!origin) {
+      return callback(null, true)
+    }
+    return callback(new Error('Not allowed by CORS2'))
+    // if (WHITELISTE.indexOf(origin) !== -1) {
+    //   callback(null, true)
+    // } else {
+    //   callback(new Error('Not allowed by CORS'))
+    // }
   }
 
 }
